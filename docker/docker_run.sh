11249b61ac26ddd1bf51985478ffa304b9f219c4
pglotfel <pglotfel@gmail.com>
2017-09-06 13:51:35 -0400

    CLi args

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --volume /home/robotarium/Git/RobotariumRepositories/robotarium_nodes/udp_service:/code/ \
+docker run --restart --volume /home/robotarium/Git/RobotariumRepositories/robotarium_nodes/udp_service:/code/ \
 	-d -p 4999:4999/udp \
-robotarium:udp_service "udp_ports.json" "192.168.1.2" "4999" \
+robotarium:udp_service "udp_ports.json" "192.168.1.2" "4999" "$1" "$2" \
 #python3 "/code/udpservice.py" "/code/udp_ports.json" "192.168.1.2" "4999"
